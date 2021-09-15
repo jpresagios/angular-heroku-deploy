93c4264e4a5e97f042e6e1c5b927ce06d029bfc4
Junior Paz Formoso <junior.presagios@gmail.com>
2021-09-16 00:13:30 +0300

    Fixed to deploy to heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 4000;
 
-app.use("/", express.static("../client/dist"));
+app.use("/", express.static("client/dist"));
 
 app.listen(port, () => {
   console.log("app is listening on port ", port);
